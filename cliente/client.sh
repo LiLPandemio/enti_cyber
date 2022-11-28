1056f277ed910dad1c4917f7c886eefde0de73cf
LiLPandemio <69552375+LiLPandemio@users.noreply.github.com>
2022-11-28 14:21:46 +0100

    Update client.sh

--- cliente/client.sh
+++ cliente/client.sh
@@ -1,39 +1,91 @@
 #!/bin/bash
+
 IP_SERVER="localhost"
 IP_LOCAL="127.0.0.1"
-PORT="42069" #Default 4242
+PORT="4242"
+MD5_IP=`echo $IP_LOCAL | md5sum | cut -d " " -f 1`
 
 echo "Cliente HMTP"
 
-echo "(1) SEND - ENVIANDO EL HANDSHAKE"
+echo "(1) ENVIANDO EL SALUDO"
 
-echo "GREEN_POWA $IP_LOCAL" | nc $IP_SERVER $PORT
+echo "GREEN_POWA $IP_LOCAL $MD5_IP" | nc $IP_SERVER $PORT 
 
-echo "(2) LISTEN - Esuchando confirmacion"
-
+echo "(2) RECIBIENDO CONFIRMACION"
 MSG=`nc -l $PORT`
 
-if ["$MSG" != "OK_HMTP"]
+if [ "$MSG" != "OK_HMTP" ]
 then 
-        echo "ERROR 1: Handshake mal formado"
-        exit 1
+	echo "ERROR 1:SALUDO MAL HECHO"
+exit 1
 fi
-echo "(5) SEND - filename (enviem el nom del arxiu)"
-FILENAME="meme.jpg"
-MD5FILESUM=`md5sum $FILENAME | cut -d " " -f 1`
-FILECONTENT=`cat $FILENAME`
-
-echo "FILE_NAME $FILENAME" | nc $IP_SERVER $PORT
-
-#Serve the file
-{echo -ne "HTTP/1.0 200 OK\r\n\r\n"; cat "$file" ;} | nc  -l "$PORT"
 
 
-echo "FILE_HASH $MD5FILESUM" | nc $IP_SERVER $PORT
+echo "(5) CONTANDO Y ENVIANDO CONTEO"
+FILE_COUNT=`ls meme/ | wc -l`
+echo "$FILE_COUNT" | nc $IP_SERVER $PORT
 
-echo "(6) LISTEN - Escoltant confirmacio del arxiu"
+echo "(6) ESCUCHANDO CONFIRMACION DE CONTEO"
+MSG=`nc -l $PORT`
+if [ "$MSG" != "OK_FILE_COUNT" ]
+then 
+echo "ERROR 2: NUMERO ERRONEO"
+exit 2
+fi
+
+echo " "
+for ((i=0; i<=$FILE_COUNT-1; i++))
+do
+FILE_NAME="ElonMusk$i.jpg"
+echo "(9) ENVIANDO MENSAJE"
+FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc $IP_SERVER $PORT
+
+echo "(10) ESCUCHANDO"
 
 MSG=`nc -l $PORT`
-echo "$MSG"
+if [ "$MSG" != "OK_FILENAME" ]
+then
+	echo "ERROR 3: EL NOMBRE DEL ARCHIVO ES ERRONEO"
+	echo "Mensaje de error: $MSG"
+exit 3
+fi
 
+echo "(13) ENVIAMOS DATOS"
+
+cat meme/$FILE_NAME | nc $IP_SERVER $PORT
+
+DATA_MD5=`cat meme/$FILE_NAME | md5sum | cut -d " " -f 1`
+
+echo "(14) ESCUCHAMOS RESPUESTA" 
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_DATA_RCPT" ]
+then
+	echo "ERROR 4: DATOS INCORRECTOS"
+	exit 4
+fi
+
+echo "(17) ENVIAMOS CONFIRMACION DE ARCHIVO"
+
+echo $DATA_MD5 | nc $IP_SERVER $PORT
+
+echo "(18) RECIBIMOS CONFIRMACION"
+
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_DATA_MD5" ]
+then
+	echo "ERROR 5: DATOS FALSOS"
+	echo "MENSAJE DE ERROR: $MSG"
+ 	exit 5
+fi
+echo " "
+done
+
+echo "DATOS ENVIADOS CORRECTAMENTE"
+
+echo "FIN DEL ENVIO"
 exit 0
