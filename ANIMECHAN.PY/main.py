825d10cf4dc8ad172a5c600443e9c78e8c072d36
LiLPandemio <markhaxx@gmail.com>
2023-06-05 01:55:33 +0200

    POR FIN FUNCIONAN LAS SCREENS DESPUES DE 10H JODER

--- ANIMECHAN.PY/main.py
+++ ANIMECHAN.PY/main.py
@@ -19,3 +19,46 @@ El menu principal tendra 4 botones centrados que dan las opciones:
             La siguiente pantalla sacara frases de un anime y te preguntara igual que en las pantallas de las otras opciones el nombre del personaje.
 
 """
+"""
+
+An implementation of a classic calculator, with a layout inspired by macOS calculator.
+
+
+"""
+from textual.app import App, ComposeResult
+from textual.widgets import Button, Label
+from textual.screen import Screen
+
+
+class AdivinaPersonajeScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield Label("Hello Textual", id="hello")
+
+
+class MainScreen(App):
+    CSS_PATH = "main.css"
+
+    def compose(self) -> ComposeResult:
+        self.close_button = Button("Close", id="close")
+        self.adivinaPersonajeLauncher = Button("Adivinar el personaje", id="adivinaPersonajeLauncher")
+        self.adivinaAnimeLauncher = Button("Adivinar el anime", id="adivinaAnimeLauncher")
+        self.adivinaPersonajeAnimeLauncher = Button("Adivinar el personaje (de un Anime)", id="adivinaPersonajeAnimeLauncher")
+
+        yield Label("Hello Textual", id="hello")
+        yield self.adivinaPersonajeLauncher
+        yield self.adivinaAnimeLauncher
+        yield self.adivinaPersonajeAnimeLauncher
+        yield self.close_button
+
+    def on_mount(self) -> None:
+        pass
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        if event.button.id == "adivinaPersonajeLauncher":
+            adivinaPersonaje = AdivinaPersonajeScreen()
+            self.push_screen(adivinaPersonaje)
+
+
+if __name__ == "__main__":
+    app = MainScreen()
+    app.run()
