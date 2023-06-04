63c94410274643edda6693a549e0701e001abfa3
LiLPandemio <markhaxx@gmail.com>
2023-06-05 00:12:07 +0200

    Planning del programa

--- ANIMECHAN.PY/main.py
+++ ANIMECHAN.PY/main.py
@@ -1,5 +1,21 @@
 """
-Proyecto de API:
-    El proyect con
+Proyecto de API: https://animechan.vercel.app/
+    El proyect consistira en un minijuego de preguntas sobre animes.
+        Adivina el personaje
+        Adivina el anime
+        Adivina el personaje del anime X
+
+Funcionamiento:
+El menu principal tendra 4 botones centrados que dan las opciones:
+    Adivina el personaje
+        El menu mostrara una frase obtenida de la api https://animechan.vercel.app/
+        Habra debajo un input text que preguntara que personaje dice esa frase.
+    Adivina el anime
+        Te saldra una frase con el nombre del personaje en el centro de la pantalla
+        Debajo un campo de texto pedirá el nombre del anime
+    Adivina el personaje del anime X
+        Aqui lo primero te preguntará de que anime quieres ver preguntas.
+        Si el anime existe pasara a la siguiente pantalla
+            La siguiente pantalla sacara frases de un anime y te preguntara igual que en las pantallas de las otras opciones el nombre del personaje.
 
 """
