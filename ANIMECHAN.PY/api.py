bc0381f4d5ee78a513ccdb4c73fbd401b5637ff0
LiLPandemio <markhaxx@gmail.com>
2023-06-11 05:22:30 +0200

    Cambio a 4chan py

---
+++ ANIMECHAN.PY/api.py
@@ -0,0 +1,42 @@
+import requests
+import asyncio
+
+# Obtener los boards
+async def get_4chan_boards():
+    url = "https://a.4cdn.org/boards.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        boards = data["boards"]
+        board_names = [board["board"] for board in boards]
+        return board_names
+    else:
+        print("Error: Failed to fetch 4chan boards.")
+        return []
+
+# # Ejemplo de uso
+# boards = get_4chan_boards()
+# print(boards)
+
+# Obtener hilos de un board
+async def get_board_threads(board):
+    url = f"https://a.4cdn.org/{board}/1.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        threads = []
+        for thread in data['threads']:
+            for post in thread['posts']:
+                threads.append(post)
+        return threads
+    else:
+        print(f"Error: Failed to fetch threads for board /{board}/")
+        return []
+
+
+
+
+# # Ejemplo de uso
+# board = "g"  # Tablero /g/ (Tecnología) de 4chan
+# threads = get_board_threads(board)
+# print(threads)
--- ANIMECHAN.PY/main.py
+++ ANIMECHAN.PY/main.py
@@ -1,210 +1,95 @@
-"""
-Proyecto de API: https://api.breakingbadquotes.xyz/v1/quotes
-    El proyect consistira en un minijuego de preguntas sobre BrakingBads.
-        Adivina el personaje
-        Adivina el personaje del BrakingBad X
-
-Funcionamiento:
-El menu principal tendra 2 botones centrados que dan las opciones:
-    Adivina el personaje
-        El menu mostrara una frase obtenida de la api https://api.breakingbadquotes.xyz/v1/quotes
-        Habra debajo un input text que preguntara que personaje dice esa frase.
-
-
-"""
 from textual.app import App, ComposeResult
-from textual.widgets import Button, Label, Input, Static
-from textual.screen import Screen
+from textual.screen import Screen  # Agrega esta línea
+from textual.widgets import Button, Label, DataTable
 import asyncio
 import httpx
 import random
-import random
 import re
+from api import get_4chan_boards, get_board_threads
+import itertools
+from bs4 import BeautifulSoup
 
+def clean_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text()
+    return text
 
-async def obtener_frase_aleatoria():
-    url = "https://api.breakingbadquotes.xyz/v1/quotes"
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url)
-            if response.status_code == 200:
-                frase = response.json()[0]
-                return frase
-            else:
-                return "Error obteniendo la frase"
-        except:
-            return "Error comunicándose con el servidor"
-
-def censurar_palabra(frase):
-    palabras = re.findall(r'\b\w+\b', frase)
-    palabras_largas = [palabra for palabra in palabras if len(palabra) > 2]  # Filtrar palabras largas
-    cantidad_palabras = len(palabras_largas)
-    palabras_a_censurar = max(1, int(cantidad_palabras * 0.5))  # Censurar al menos una palabra
-    palabras_aleatorias = random.sample(palabras_largas, palabras_a_censurar)
-
-    palabra_a_censurar = random.choice(palabras_aleatorias)
-    palabra_oculta = "*" * len(palabra_a_censurar)
-    frase_censurada = re.sub(r'\b' + re.escape(palabra_a_censurar) + r'\b', palabra_oculta, frase)
-
-    return palabra_a_censurar, frase_censurada
-
-class AdivinaPersonajeScreen(Screen):
+class displayBoardScreen(Screen):
     CSS_PATH = "main.css"
-    def compose(self) -> ComposeResult:
-                # Definición del botón de cerrar juego
-        self.close_button = Button("Salir", classes="danger", id="close")
-        self.guessinput = Input(placeholder="Respuesta")
-        # Creando un elemento static al que hacer update
-        self.frase_aleatoria = Static(id="frase_aleatoria")
-        self.frase_aleatoria.update("Loading...")
-        # Creación de los objetos
-        yield Label("Bienvenido a Breaking Bad Guesser", id="hello")
-        yield self.frase_aleatoria
-        self.backbutton = Button("Volver al menú", classes="danger", id="goBack")
-        self.submitButton = Button("Responder", classes="success", id="submit")
-        yield self.guessinput
-        self.pasapalabra = Button("Pasa palabra", id="newquestion")
-        yield self.submitButton
-        yield self.pasapalabra
-        # Boton ir atrás
-        self.backbutton = Button("Volver al menú", classes="danger", id="goBack")
-        yield self.backbutton
-    async def obtener_y_actualizar_frase_aleatoria(self):
-        frase_aleatoria = await obtener_frase_aleatoria()
-        frase = frase_aleatoria["quote"]
-        self.answer = frase_aleatoria["author"]
-        if frase is not None:
-            self.frase_aleatoria.update(frase)   # Update the frase_aleatoria_text property
-        else:
-            self.frase_aleatoria.update("Error")  # Update the frase_aleatoria_text property
-        self.refresh()
-    async def on_mount(self):
-        await self.obtener_y_actualizar_frase_aleatoria()
 
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
+    def __init__(self, board):
+        super().__init__()
+        self.board = board
+
+    def compose(self) -> ComposeResult:
+        yield Label(f"Welcome to {self.board} board!", id="welcome")  # Display the board name
+        
+        yield DataTable()
+
+        
+        
+        yield Button("Back", id="goBack", classes="danger")  # Example button widget
+
+    async def on_mount(self) -> None:
+        table = self.query_one(DataTable)
+        table.overflow_x = 'hidden'
+        table.focus()
+        table.add_columns("Post id", "Replies", "Poster", "Content")
+        threads = await get_board_threads(self.board)
+        for thread in itertools.islice(threads, 0, 100):
+            table.add_row(thread["no"], thread.get("replies", "0"), f"{thread['name']}", clean_html(thread.get("com", ""))[:120]+"...")
+        table.fixed_rows = 2
+        table.fixed_columns = 1
+        table.cursor_type = "row"
+        table.zebra_stripes = True
+
+    def on_button_pressed(self, event):
         # Lógica del botón de salir
         if event.button.id == "goBack":
             self.dismiss()  # Pop screen
-        # Lógica del botón submit
-        if event.button.id == "newquestion":
-            await self.obtener_y_actualizar_frase_aleatoria()
-        if event.button.id == "submit":
-            text = self.guessinput.value
-            if(text == self.answer):
-                text = "Tu respuesta es correcta"
-            else:
-                text = "La respuesta correcta era: " + self.answer 
-            self.frase_aleatoria.update(text)
 
-class AdivinaPalabraScreen(Screen):
-    CSS_PATH = "main.css"
-
-
-    def compose(self) -> ComposeResult:
-        self.answer = ""
-
-        # Creando un elemento static para mostrar la frase
-        self.frase = Static(id="frase")
-        self.frase.update("Loading...")
-        yield Label("Bienvenido a Breaking Bad Guesser, ADIVINA LA PALABRA!!!!!!!", id="hello")
-        yield self.frase
-
-        # Creando un elemento input para que el usuario ingrese su respuesta
-        self.guessinput = Input(placeholder="Respuesta")
-        yield self.guessinput
-
-        # Botón para enviar la respuesta
-        self.submitButton = Button("Responder", classes="success", id="submit")
-        yield self.submitButton
-
-        # Botón para obtener una nueva frase
-        self.newFraseButton = Button("Nueva Frase", id="newFrase")
-        yield self.newFraseButton
-
-        # Boton ir atrás
-        self.backbutton = Button("Volver al menú", classes="danger", id="goBack")
-        yield self.backbutton
-
-    async def obtener_y_actualizar_frase_aleatoria(self):
-        frase_aleatoria = await obtener_frase_aleatoria()
-        self.answer, frase = censurar_palabra(frase_aleatoria["quote"])
-        if frase is not None:
-            self.frase.update(frase + "\n -  " + frase_aleatoria["author"])  # Update the frase_text property
-        else:
-            self.frase.update("Error")  # Update the frase_text property
-        self.refresh()
-
-    async def on_mount(self):
-        await self.obtener_y_actualizar_frase_aleatoria()
-
-    async def on_button_pressed(self, event: Button.Pressed) -> None:
-        # Lógica del botón de volver al menú
-        if event.button.id == "goBack":
-            self.dismiss()  # Pop screen
-
-        # Lógica del botón de obtener nueva frase
-        if event.button.id == "newFrase":
-            await self.obtener_y_actualizar_frase_aleatoria()
-
-        # Lógica del botón de submit
-        if event.button.id == "submit":
-            respuesta_usuario = self.guessinput.value
-            if respuesta_usuario == self.answer:
-                resultado = "¡Respuesta correcta!"
-            else:
-                resultado = "Respuesta incorrecta. La respuesta correcta era: " + self.answer
-            self.frase.update(resultado)
-
-
-class MyFuckingScreen(App):
+class displayThreadScreen(Screen):
     CSS_PATH = "main.css"
 
     def compose(self) -> ComposeResult:
-        # Definición de los botones de lanzamiento
-        self.Launch_AdivinaPersonaje = Button("Adivina el personaje", id="Launch_AdivinaPersonaje")
-        self.Launch_AdivinaPalabra = Button("Adivina la palabra", id="Launch_AdivinaPalabra")
-
-        # Definición del botón de cerrar juego
         self.close_button = Button("Salir", classes="danger", id="close")
 
-        # Creando un elemento static al que hacer update
-        self.frase_aleatoria = Static(id="frase_aleatoria", classes="text")
-        self.frase_aleatoria.update("Loading...")
-        # Creación de los objetos
-        yield Label("Bienvenido a Breaking Bad Guesser", id="hello")
-        yield self.frase_aleatoria
-        yield self.Launch_AdivinaPersonaje
-        yield self.Launch_AdivinaPalabra
-        yield self.close_button
 
-    async def obtener_y_actualizar_frase_aleatoria(self):
-        frase_aleatoria = await obtener_frase_aleatoria()
-        frase_aleatoria = frase_aleatoria["quote"] + "\n  - " + frase_aleatoria["author"]
-        if frase_aleatoria is not None:
-            self.frase_aleatoria.update(frase_aleatoria)  # Update the frase_aleatoria_text property
-        else:
-            self.frase_aleatoria.update("Error")  # Update the frase_aleatoria_text property
-        self.refresh()
+class MainScreen(App):
+    CSS_PATH = "main.css"
 
-    async def on_mount(self):
-        await self.obtener_y_actualizar_frase_aleatoria()
+    def __init__(self, boards):
+        super().__init__()
+        self.boards = boards
+        self.buttons = []  # Lista para almacenar los botones
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        # Lógica del botón de salir
-        if event.button.id == "close":
-            self.exit()
+        for board in self.boards:
+            self.buttons.append(Button(board, id=f"board_{board}"))
 
-        # Lógica del botón de Adivinar el personaje
-        if event.button.id == "Launch_AdivinaPersonaje":
-            adivinaPersonajeScreen = AdivinaPersonajeScreen()
-            self.push_screen(adivinaPersonajeScreen)
+    def compose(self) -> ComposeResult:
+        yield Label("Bienvenido a 4chan, selecciona un board", id="welcome")
 
-        # Lógica del botón de Adivinar la palabra
-        if event.button.id == "Launch_AdivinaPalabra":
-            adivinaPalabraScreen = AdivinaPalabraScreen()
-            self.push_screen(adivinaPalabraScreen)
+        for button in self.buttons:  # Iterar sobre la lista de botones
+            yield button
+
+    def on_button_pressed(self, event):
+        board = event.button.id.split("_")[1]
+        print(f"Botón {board} presionado")
+        # Abrir displayBoardScreen y pasar el parámetro 'board'
+        self.push_screen(displayBoardScreen(board))
+
+
+async def get_4chan_boards_async():
+    return await get_4chan_boards()
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    boards = loop.run_until_complete(get_4chan_boards_async())  # Obtener la lista de boards de manera sincrónica
+
+    app = MainScreen(boards=boards)  # Pasar la lista de boards como parámetro
+    app.run()
 
 
 if __name__ == "__main__":
-    app = MyFuckingScreen()
-    app.run()
+    main()
